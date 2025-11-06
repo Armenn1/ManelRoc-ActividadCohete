276b108efa63f825490935e498bfeaf1e743d5b8
Armenn1 <armengolllll@gmail.com>
2025-11-06 16:58:43 +0100

    create classes

---
+++ ManelRoc-ActividadCohete/ManelRoc-ActividadCohete/Cohete.cpp
@@ -0,0 +1,6 @@
+#include "Cohete.h"
+
+Cohete::AgregarCap()
+{
+	cout << "Agregando cápsula al cohete " << id << endl;
+}
---
+++ ManelRoc-ActividadCohete/ManelRoc-ActividadCohete/Cohete.h
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#pragma once
+
+// Un cohete tiene un identificador, un destino, la distancia al destino y un compartimento que puede alojar hasta 10 cápsulas de energía.Cada 100.000 Km consumen 10.000KWh de energía.
+// Representa el compartimento de las cápsulas como un array de 10 apuntadores a objetos cápsula.
+// En caso de que un compartimento no tenga ninguna cápsula su apuntador tiene que tomar como valor nullptr.
+// Incluye al conjunto de miembros de datos de la clase cohete un número que indique la cantidad de cápsulas que aloja en un momento determinado.
+
+class Cohete
+{
+	
+protected:
+	int id;
+
+
+	
+public:
+	AgregarCap();
+	EliminarCap();
+	TransferirCap();
+	ValidarViaje();
+	Viajar();
+	Cargar();
+};
+
